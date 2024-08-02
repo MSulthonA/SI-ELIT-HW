75d81133c83f993da7f8a9e0e9a9c71706698e1a
Muhammad Sulthon Auliya <muhammadsulthonauliya313@gmail.com>
2024-08-02 08:16:43 +0700

    Update SIELIT_CODEv2.ino

--- SIELIT_CODEv2.ino
+++ SIELIT_CODEv2.ino
@@ -1,7 +1,24 @@
-// Setting Perangkat
+/*** Ini adalah Bagian kode utama dari SIELIT
+Disini dijelaskan pemanggilan fungsi2 yang ada di masing-masing file tiap komponen
+ada beberapa file komponen yang dipanggil mulai dari BATERRY_CONFIG.ino, EEPROM_CONFIG, 
+LCD_CONFIG, LED_BUZZER, RFID-CONFIG, ROTARY_CONFIG, SPIFSS_CONFIG, dan WIFI_MQTT_CONFIG.
+
+Kode ini menggunakan prinsip RTOS menggunakan 2 core dari ESP32:
+Core 0 : taskkontrol
+Core 1 : taskwifi
+Kode ini menerapkan 2 mode:
+- Mode Konfigurasi
+- Mode Kontrol
+Kode ini menghandling 2 kondisi : 
+- Offline ( tidak terhubung atau hanya terhubung wifi )
+- Online (terhubung ke server & wifi)
+***/
+
+// Setting Perangkat 
+// Ganti nama dan topik tiap perangkat bila ingin memulai pengkodean perangkat tertentu
 const char* namaPerangkat = "SIELIT_B";
-const char* Topic = "PegonBacaanP";
-const char* credential[14] = {"SAS", "123456789",                  
+const char* Topic = "PegonBacaanP";   // Untuk terhubung ke MQTT broker
+const char* credential[14] = {"SAS", "123456789",                      // Pastikan bila mengubah ini, jumlah array sama dengan inisialisasi
                               "PPMBKI ASTRA 1", "bki12345678",
                               "PPMBKI ASTRA 2", "calonmenantuidaman",
                               "PPMBKI ASTRI 3", "",
@@ -9,21 +26,23 @@ const char* credential[14] = {"SAS", "123456789",
                               "PPMBKI ASTRI 1", "istrisolekha",
                               "PPMBKI ASTRI 2", "istrisolekha"
                             };
-int len = *(&credential + 1) - credential;
+int len = *(&credential + 1) - credential; // Sebagai pangjang array credential
 
 
 //End Of Settingan Perangkat
 
-#define MENU_BUTTON 34
+// Button merah untuk menggati mode
+#define MENU_BUTTON 34 
 
+// inisialisasi task
 TaskHandle_t taskkontrol;
 TaskHandle_t taskwifi;
 
 // MODE ditriggrer menggunakan interrupt
 bool MODE = false; // true : mode config, false : mode operation
-bool LAST_MODE = MODE;
-bool isConnect = false;
-uint8_t KELAS = 2; // 0:PB 1:LB 2:CP 3:SR
+bool LAST_MODE = MODE; // menyimpan riwayat mode
+bool isConnect = false; // Apakah online atau offline
+uint8_t KELAS = 2; // 0:PB 1:LB 2:CP 3:SR // Tidak dipakai
 
 //variables to keep track of the timing of recent interrupts
 unsigned long button_time = 0;  
@@ -32,13 +51,14 @@ unsigned long last_button_time = 0;
 uint32_t start_mil = 0; 
 uint32_t start_req = 0; 
 
+// Semaprore, fitur untuk menghindari Racing data pada LCD
 SemaphoreHandle_t xMutex = NULL;
-
+// Untuk Passing data antar task kontrol dan task wifi
 QueueHandle_t queue;
-
 
 char* reply;
 
+// Keperluan testing
 void getTest(){
   Serial.print("Heap : ");
   Serial.println(ESP.getHeapSize());
@@ -48,6 +68,7 @@ void getTest(){
   Serial.println(ESP.getCycleCount());
 }
 
+// Handling interupt dari button
 void IRAM_ATTR ISR_changeMenu(){
     button_time = millis();
     if(button_time-last_button_time > 250){ //250
@@ -61,6 +82,7 @@ void IRAM_ATTR ISR_changeMenu(){
     }
 }
 
+// tempat inisialisasi tiap task dan beberapa tambahan
 void setup() {
   start_mil = millis();
   Serial.begin(115200);
@@ -106,9 +128,8 @@ void task_kontrol( void * pvParameters ){
   char* offbuffer;
 //  uint32_t start = millis();
   
-  //Tambahan
+  // Void setupnya RTOS: inisialisasi beberapan komponene
   SPIFFS_init();
-  //LCD
   LCD_initt();
   LCD_wait();
   RFID_init();
@@ -117,14 +138,14 @@ void task_kontrol( void * pvParameters ){
   get_Battery();
   LED_BUZZER_init();
   pinMode(MENU_BUTTON, INPUT);
-  attachInterrupt(MENU_BUTTON, ISR_changeMenu, RISING);
+  attachInterrupt(MENU_BUTTON, ISR_changeMenu, RISING); // Init Button menu
   uint32_t start_publish = millis();
   
-  
+  //Void loop nya RTOS
   for(;;){
     changeMenu();
     switch(MODE){
-    case true: // MODE 
+    case true: // MODE Konfigurasi
       ROTARY_config();
       MENU_CONFIG();
       break;
@@ -136,7 +157,7 @@ void task_kontrol( void * pvParameters ){
            MQTT_publish(datas);
 //           vTaskDelay(1000);
         }
-        // setiap 20 detik dicek
+        // setiap 20 detik dicek kalau ada data di SPIFSS maka akan pusblish MQTT
         if((millis()-start_publish) >= 20000){
             //dibaca dulu
             offbuffer = readPresence();
@@ -161,7 +182,7 @@ void task_kontrol( void * pvParameters ){
             }
             start_publish = millis();
         }
-      }else{ // jika offline
+      }else{ // jika offline : menammbahkan data pembacaan RFID
         if(datas!='\0'){
             //ngirim ke spiffs
             char failMSG[10];
@@ -172,6 +193,7 @@ void task_kontrol( void * pvParameters ){
             LCD_process(datas);
         }
       }
+      // Menerima data dari task wifi untuk ditampilkan di LCD
       char token[45];
       if(xQueueReceive(queue, &messageTemp, xTicksToWait)== pdPASS){
         Serial.print("Pesan :");
@@ -196,27 +218,30 @@ void task_kontrol( void * pvParameters ){
   }
 }
 
-
+// Task WIFI untuk mengurus pengiriman data menggunakan protokol MQTT
 void task_wifi( void * pvParameters ){
   Serial.print( "task wifi running on: core " );
   Serial.println(xPortGetCoreID() );
 
-  //Tambahan
-  EEPROM_init();
+  //Void setupnya RTOS
+  EEPROM_init(); // EEPROM dipakai dikedua task tapi diinit ke sini karena Wifi butuh duluan
   MQTT_init();
-  
+
+  //Void loopnya RTOS
   for(;;){
     MQTT_config();
     vTaskDelay(100);
   }
 }
 
+// Nggak kepake
 void loop(){
 //Serial.print( " loop() is running on: Core " );
 //Serial.println( xPortGetCoreID() );
 //delay(1000);
 }
 
+//Fungsi handling LCD ketika ganti menu
 void changeMenu(){
   if(MODE != LAST_MODE){
     if(MODE){
